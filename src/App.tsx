aada5be2d3e9b75c4686332dbbe8e871b808684d
sujanthapa01 <sujanthapa4001@gmail.com>
2025-07-22 01:11:16 +0530

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Docs from "./pages/docs";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,7 +17,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/docs" element={<Docs />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
