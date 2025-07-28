2a73af9f05640d62b482850bfa90370ec228afc3
sujanthapa01 <sujanthapa4001@gmail.com>
2025-07-28 22:14:32 +0530

    fix: remove docs form main repo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Docs from "./pages/docs";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,7 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/docs" element={<Docs />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
